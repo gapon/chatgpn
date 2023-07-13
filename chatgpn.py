17f651fda22f2b3fa11eec82a6559e15e8a1c10e
Eugene Gapon <eugenegapon@gmail.com>
2023-07-13 16:48:45 +0300

    Change port

--- chatgpn.py
+++ chatgpn.py
@@ -12,7 +12,7 @@ print(TOKEN)
 
 if BOT_ENV == 'prod':
     APP_NAME = 'https://gapon.me/'
-    PORT = int(os.environ.get('PORT', '8443'))
+    PORT = int(os.environ.get('PORT', '8444'))
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -20,23 +20,22 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    await update.message.reply_text(f'Hello {user}')
+    await update.message.reply_text(f'Hello {user.first_name}')
 
 
 def main() -> None:
-    """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(TOKEN).build()
 
-    # on different commands - answer in Telegram
+    # On different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
 
     # Run the bot until the user presses Ctrl-C
     if BOT_ENV == 'prod':
         application.start_webhook(listen="0.0.0.0",
-                              port=PORT,
-                              url_path=TOKEN,
-                              webhook_url = APP_NAME + TOKEN)
+            port=PORT,
+            url_path=TOKEN,
+            webhook_url = APP_NAME + TOKEN)
     else:
         application.run_polling(allowed_updates=Update.ALL_TYPES)
 
